2bc12299c851695c00ac3376eb538aea3e59b3ae
zNotJhoan <striderhosting69@gmail.com>
2021-11-09 15:41:40 -0500

    [%] Fixed interactionCreate Event

--- events/handler/interactionCreate.js
+++ events/handler/interactionCreate.js
@@ -3,8 +3,6 @@ const client = require('../../index');
 client.on("interactionCreate", async (interaction) => {
     // Slash Command Handling
     if (interaction.isCommand()) {
-        await interaction.deferReply({ ephemeral: false }).catch(() => {});
-
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd)
             return interaction.followUp({ content: "An error has occured " });
