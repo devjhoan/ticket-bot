09c7baa1cb736912af78a90afd7a6038827c9271
zNotJhoan <striderhosting69@gmail.com>
2021-11-19 11:45:45 -0500

    [%] Add command adapted to use of mongoose

--- commands/tickets/add.js
+++ commands/tickets/add.js
@@ -2,6 +2,7 @@ const { Client, Message, MessageEmbed } = require('discord.js');
 const config = require('../../config/config.json')
 const enable = require('../../config/booleans.json')
 const mensajes = require('../../config/messages.json');
+const ticketSchema = require("../../models/ticketSchema");
 
 module.exports = {
   name: "add",
@@ -15,12 +16,21 @@ module.exports = {
   run: async (client, message, args) => {
     if(enable.COMMANDS.ADD === false) return;
     if(!message.member.roles.cache.get(config.TICKET['STAFF-ROLE'])) return message.channel.send({content: `${mensajes['NO-PERMS']}`}).then((msg) =>
-    setTimeout(() => {
-        msg.delete()
-    }, 5000)
-);
-    if(message.channel.parentId !== config['TICKET-PANEL'].CATEGORY) return message.channel.send({content: mensajes['NO-TICKET']})
+    setTimeout(() => {msg.delete()}, 5000));
+    
+    const guildData = await ticketSchema.findOne({guildID: message.guild.id})
+    if(!guildData) return message.channel.send({content: mensajes['NO-SERVER-FIND']}).then((msg) =>
+    setTimeout(() => {msg.delete() }, 5000));
+    if(!guildData.tickets || guildData.tickets.length === 0) return message.channel.send({content: mensajes['NO-TICKET-FIND']}).then((msg) =>
+    setTimeout(() => {msg.delete()}, 5000));
+    const ticketData = guildData.tickets.map(z  => { return { customID: z.customID, ticketName: z.ticketName, ticketFooter: z.ticketFooter, ticketCategory: z.ticketCategory, ticketEmoji: z.ticketEmoji,}})
+    const categoryID = ticketData.map(x => {return x.ticketCategory})
+    if(!categoryID.includes(message.channel.parentId)) return message.channel.send({content: mensajes['NO-TICKET']}).then((msg) =>
+    setTimeout(() => {msg.delete()}, 5000));
+
     let user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+    if(!user) return message.channel.send({content: "Hey, you haven't specified the user you want add"}).then((msg) =>
+    setTimeout(() => {msg.delete() }, 5000));
     let añadido = user.id;
     const embed2 = new MessageEmbed()
       .setDescription("```"+ mensajes['NO-TICKET-ADD'] +"```")
@@ -40,8 +50,11 @@ module.exports = {
     message.channel.send({
         embeds: [embed]
     })
+    if(!guildData) return interaction.reply({content: `${mensajes['NO-SERVER-FIND']}`, ephemeral: true})
+    let logcanal = guildData.channelLog;
+    if(!logcanal) return;
     if(config.TICKET["LOGS-SYSTEM"] == true) {
-      client.channels.cache.get(config.TICKET["LOG-CHANNEL"]).send(
+      client.channels.cache.get(logcanal).send(
         {embeds: [new MessageEmbed()
             .setAuthor(""+config.TICKET["SERVER-NAME"]+" | Add Member", "https://emoji.gg/assets/emoji/9846-discord-stage.png")
             .setColor("GREEN")
