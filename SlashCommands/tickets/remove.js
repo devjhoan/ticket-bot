016743539aae317f3e581cff4b13544e06b80074
zNotJhoan <striderhosting69@gmail.com>
2021-11-19 11:55:00 -0500

    [%] Remove slash command adapted to use of mongoose

--- SlashCommands/tickets/remove.js
+++ SlashCommands/tickets/remove.js
@@ -2,7 +2,7 @@ const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
 const config = require('../../config/config.json')
 const enable = require('../../config/booleans.json')
 const mensajes = require('../../config/messages.json');
-
+const ticketSchema = require("../../models/ticketSchema");
 module.exports = {
     name: "remove",
     description: "remove a user of a ticket",
@@ -24,7 +24,14 @@ module.exports = {
     run: async (client, interaction, args) => {
         if(enable.COMMANDS.REMOVE === false) return;
         if(!interaction.member.roles.cache.get(config.TICKET['STAFF-ROLE'])) return interaction.reply({content: `${mensajes['NO-PERMS']}`, ephemeral: true})
-        if(interaction.channel.parentId !== config['TICKET-PANEL'].CATEGORY) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
+        
+        const guildData = await ticketSchema.findOne({guildID: interaction.guild.id})
+        if(!guildData) return interaction.reply({content: mensajes['NO-SERVER-FIND'], ephemeral: true})
+        if(!guildData.tickets || guildData.tickets.length === 0) return interaction.reply({content: mensajes['NO-TICKET-FIND'], ephemeral: true})
+        const ticketData = guildData.tickets.map(z  => { return { customID: z.customID, ticketName: z.ticketName, ticketFooter: z.ticketFooter, ticketCategory: z.ticketCategory, ticketEmoji: z.ticketEmoji,}})
+        const categoryID = ticketData.map(x => {return x.ticketCategory})
+        if(!categoryID.includes(interaction.channel.parentId)) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
+
         let si = interaction.options.getUser('user');
         let removido = si.id;
         const embed2 = new MessageEmbed()
@@ -42,9 +49,12 @@ module.exports = {
         interaction.reply({
             embeds: [embed]
         })
+        if(!guildData) return interaction.reply({content: `${mensajes['NO-SERVER-FIND']}`, ephemeral: true})
+        let logcanal = guildData.channelLog;
+        if(!logcanal) return;
         if(config.TICKET["LOGS-SYSTEM"] == true) {
-          client.channels.cache.get(config.TICKET["LOG-CHANNEL"]).send(
-            {embeds: [new MessageEmbed()
+          client.channels.cache.get(logcanal).send({
+            embeds: [new MessageEmbed()
                 .setAuthor(""+config.TICKET["SERVER-NAME"]+" | Remove Member", "https://emoji.gg/assets/emoji/1590_removed.png")
                 .setColor("RED")
                 .setDescription(`
