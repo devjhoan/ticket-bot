9c7f18d9ed17a95f22bf7126fc479a65e31e408f
zNotJhoan <striderhosting69@gmail.com>
2021-11-19 11:54:27 -0500

    [%] Alert slash command adapted to use of mongoose

--- SlashCommands/tickets/alert.js
+++ SlashCommands/tickets/alert.js
@@ -2,7 +2,7 @@ const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
 const config = require('../../config/config.json')
 const enable = require('../../config/booleans.json')
 const mensajes = require('../../config/messages.json');
-
+const ticketSchema = require("../../models/ticketSchema");
 module.exports = {
     name: "alert",
     description: "Notify a user that their ticket will be closed",
@@ -24,10 +24,16 @@ module.exports = {
     run: async (client, interaction, args) => {
     if(enable.COMMANDS.ALERT === false) return;
     if(!interaction.member.roles.cache.get(config.TICKET['ADMIN-ROLE'])) return interaction.reply({content: `${mensajes['NO-PERMS']}`, ephemeral: true})
-    if(interaction.channel.parentId !== config['TICKET-PANEL'].CATEGORY) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
-    let si = interaction.options.getUser('user');
-    let user = si;
-    if(!si) {
+    
+    const guildData = await ticketSchema.findOne({guildID: interaction.guild.id})
+    if(!guildData) return interaction.reply({content: mensajes['NO-SERVER-FIND'], ephemeral: true})
+    if(!guildData.tickets || guildData.tickets.length === 0) return interaction.reply({content: mensajes['NO-TICKET-FIND'], ephemeral: true})
+    const ticketData = guildData.tickets.map(z  => { return { customID: z.customID, ticketName: z.ticketName, ticketFooter: z.ticketFooter, ticketCategory: z.ticketCategory, ticketEmoji: z.ticketEmoji,}})
+    const categoryID = ticketData.map(x => {return x.ticketCategory})
+    if(!categoryID.includes(interaction.channel.parentId)) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
+    
+    let user = interaction.options.getUser('user');
+    if(!user) {
         return interaction.reply({
             embeds: [new MessageEmbed().setDescription("\❌ Hey, no has mencionado a la persona!").setColor("RED")]
         })
@@ -39,8 +45,11 @@ module.exports = {
     interaction.reply({
         embeds: [new MessageEmbed().setDescription("\✅ He avisado a "+ user.username +" correctamente!").setColor("GREEN")]
     })
+        if(!guildData) return interaction.reply({content: `${mensajes['NO-SERVER-FIND']}`, ephemeral: true})
+        let logcanal = guildData.channelLog;
+        if(!logcanal) return;
     if(config.TICKET["LOGS-SYSTEM"] == true) {
-        client.channels.cache.get(config.TICKET["LOG-CHANNEL"]).send(
+        client.channels.cache.get(logcanal).send(
             {embeds: [new MessageEmbed()
                 .setAuthor(""+config.TICKET["SERVER-NAME"]+" | Alert Member", "https://emoji.gg/assets/emoji/6773_Alert.png")
                 .setColor("ORANGE")
