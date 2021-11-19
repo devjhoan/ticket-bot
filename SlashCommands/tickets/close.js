02a70785455c940a93ee4f6f60c63acb25077096
zNotJhoan <striderhosting69@gmail.com>
2021-11-19 11:54:45 -0500

    [%] Close slash command adapted to use of mongoose

--- SlashCommands/tickets/close.js
+++ SlashCommands/tickets/close.js
@@ -2,6 +2,8 @@ const { Client, CommandInteraction, MessageEmbed, MessageActionRow, MessageButto
 const enable = require('../../config/booleans.json')
 const config = require('../../config/config.json')
 const mensajes = require('../../config/messages.json');
+const ticketSchema = require("../../models/ticketSchema");
+
 if(enable.COMMANDS.CLOSE === false) return;
 
 module.exports = {
@@ -16,7 +18,14 @@ module.exports = {
      */
     run: async (client, interaction, args) => {
     if(!interaction.member.roles.cache.get(config.TICKET['STAFF-ROLE'])) return interaction.reply({content: `You don't have permissions!`, ephemeral: true})
-    if(interaction.channel.parentId !== config['TICKET-PANEL'].CATEGORY) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
+    
+    const guildData = await ticketSchema.findOne({guildID: interaction.guild.id})
+    if(!guildData) return interaction.reply({content: mensajes['NO-SERVER-FIND'], ephemeral: true})
+    if(!guildData.tickets || guildData.tickets.length === 0) return interaction.reply({content: mensajes['NO-TICKET-FIND'], ephemeral: true})
+    const ticketData = guildData.tickets.map(z  => { return { customID: z.customID, ticketName: z.ticketName, ticketFooter: z.ticketFooter, ticketCategory: z.ticketCategory, ticketEmoji: z.ticketEmoji,}})
+    const categoryID = ticketData.map(x => {return x.ticketCategory})
+    if(!categoryID.includes(interaction.channel.parentId)) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
+
     const idmiembro = interaction.channel.topic;
     const embed = new MessageEmbed()
         .setDescription("```Support team ticket controls```")
