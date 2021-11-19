e6c1009254ede64652c9e4e26ded401ee3128935
zNotJhoan <striderhosting69@gmail.com>
2021-11-19 11:55:11 -0500

    [%] Rename slash command adapted to use of mongoose

--- SlashCommands/tickets/rename.js
+++ SlashCommands/tickets/rename.js
@@ -2,7 +2,7 @@ const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
 const config = require('../../config/config.json')
 const enable = require('../../config/booleans.json')
 const mensajes = require('../../config/messages.json');
-
+const ticketSchema = require("../../models/ticketSchema");
 module.exports = {
     name: "rename",
     description: "rename the name of a ticket channel",
@@ -24,7 +24,14 @@ module.exports = {
     run: async (client, interaction, args) => {
         if(enable.COMMANDS.RENAME === false) return;
         if(!interaction.member.roles.cache.get(config.TICKET['STAFF-ROLE'])) return interaction.reply({content: `${mensajes['NO-PERMS']}`, ephemeral: true})
-        if(interaction.channel.parentId !== config['TICKET-PANEL'].CATEGORY) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
+        
+        const guildData = await ticketSchema.findOne({guildID: interaction.guild.id})
+        if(!guildData) return interaction.reply({content: mensajes['NO-SERVER-FIND'], ephemeral: true})
+        if(!guildData.tickets || guildData.tickets.length === 0) return interaction.reply({content: mensajes['NO-TICKET-FIND'], ephemeral: true})
+        const ticketData = guildData.tickets.map(z  => { return { customID: z.customID, ticketName: z.ticketName, ticketFooter: z.ticketFooter, ticketCategory: z.ticketCategory, ticketEmoji: z.ticketEmoji,}})
+        const categoryID = ticketData.map(x => {return x.ticketCategory})
+        if(!categoryID.includes(interaction.channel.parentId)) return interaction.reply({content: mensajes['NO-TICKET'], ephemeral: true})
+        
         let newName = interaction.options.getString('name');
         let channel = interaction.channel;
         if(!newName) {
@@ -38,9 +45,12 @@ module.exports = {
         .setDescription("```"+ mensajes['TICKET-RENAMED'] +" ticket-"+ newName +"```")
         .setColor("GREEN")
         interaction.reply({embeds:[renameado]})
+        if(!guildData) return interaction.reply({content: `${mensajes['NO-SERVER-FIND']}`, ephemeral: true})
+        let logcanal = guildData.channelLog;
+        if(!logcanal) return;
         if(config.TICKET["LOGS-SYSTEM"] == true) {
-          client.channels.cache.get(config.TICKET["LOG-CHANNEL"]).send(
-            {embeds: [new MessageEmbed()
+          client.channels.cache.get(logcanal).send({
+            embeds: [new MessageEmbed()
                 .setAuthor(""+config.TICKET["SERVER-NAME"]+" | Rename Ticket", "https://emoji.gg/assets/emoji/9557-pencil.png")
                 .setColor("DARK_VIVID_PINK")
                 .setDescription(`
