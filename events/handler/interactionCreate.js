0bebe88bfda21a4c29e3d77b1ccf341f03bd4fc3
zNotJhoan <striderhosting69@gmail.com>
2021-11-28 21:43:09 -0500

    [%] Fixed error when no exist a slash command

--- events/handler/interactionCreate.js
+++ events/handler/interactionCreate.js
@@ -5,7 +5,7 @@ client.on("interactionCreate", async (interaction) => {
     if (interaction.isCommand()) {
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd)
-            return interaction.followUp({ content: "An error has occured " });
+            return interaction.reply({ content: "An error has occured " });
 
         const args = [];
 
