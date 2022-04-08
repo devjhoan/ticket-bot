bc88e2df0e4cda4fba9cf88383680e3dbfc43b70
DevJhoan <znotjhoan@gmail.com>
2022-04-08 16:24:18 -0500

    remove: dupiclated event

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-const { success } = require('../controllers/logger');
-const client = require('../index');
-
-client.on("ready", (client_) =>  {
-    success(client_.languages.__("system.bot_ready"));
-});
